1ab04fc9b257efbfa024f6802b7a2e03ab0bd06f
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2022-08-26 08:31:04 -0400

    Add project files.

---
+++ CertGenerator/CertTool.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace CertGenerator
+{
+    /// <summary>
+    /// Manages certificates used by this app.
+    /// </summary>
+    static class CertificateTool
+    {
+        static public X509Certificate2 CreateSelfSigned(string dnsName, int years = 10)
+        {
+            using (var rsa = RSA.Create(2048))
+            {
+                var req = new CertificateRequest($"CN={dnsName}", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                AddHttpsUsage(req, dnsName);
+                var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(years));
+                return cert;
+            }
+        }
+        static void AddHttpsUsage(CertificateRequest req, string dnsName)
+        {
+            //req.CertificateExtensions.Add(
+            //    new X509BasicConstraintsExtension(false, false, 0, false));
+
+            req.CertificateExtensions.Add(
+                new X509KeyUsageExtension(
+                    X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment,
+                    false));
+
+            req.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(req.PublicKey, false));
+
+            req.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection
+            {
+                new Oid("1.3.6.1.5.5.7.3.1") // server auth
+                //new Oid("1.3.6.1.5.5.7.3.8") // timestamp
+            }, false));
+
+            var san = new SubjectAlternativeNameBuilder();
+            //san.AddIpAddress(IPAddress.Loopback);
+            //san.AddIpAddress(IPAddress.IPv6Loopback);
+            san.AddDnsName(dnsName);
+            req.CertificateExtensions.Add(san.Build());
+        }
+
+
+        public static void SavePublicPart(X509Certificate2 cert, string filePath)
+        {
+            var pubCertBytes = cert.Export(X509ContentType.Cert);
+            // Create Base 64 encoded public cert file
+            File.WriteAllText(filePath,
+                "-----BEGIN CERTIFICATE-----\r\n"
+                + Convert.ToBase64String(pubCertBytes, Base64FormattingOptions.InsertLineBreaks)
+                + "\r\n-----END CERTIFICATE-----");
+        }
+    }
+}
---
+++ CertGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using CertGenerator;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Call this with the cert host name to generate it.");
+}
+else
+{
+    var name = args[0];
+    using (var cert = CertificateTool.CreateSelfSigned(name))
+    {
+        CertificateTool.SavePublicPart(cert, $"{name}.crt");
+        File.WriteAllBytes($"{name}.pfx", cert.Export(System.Security.Cryptography.X509Certificates.X509ContentType.Pfx));
+    }
+    Console.WriteLine("Generated!");
+}
